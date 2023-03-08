7fd7ba92cee9ec890fe262e6122e74e1dd9d662d
kpa <billkp@yeah.net>
2023-03-08 18:35:31 +0800

    更新服务端代码

--- server/webSocket.js
+++ server/webSocket.js
@@ -1,24 +1,59 @@
 const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
 
-// 创建 WebSocket 服务器
-const server = new WebSocket.Server({ port: 8080 });
+// 存储已验证的客户端
+const clients = new Set();
 
-// 监听连接事件
-server.on('connection', (socket) => {
+// 发送心跳包
+function heartbeat() {
+  clients.forEach((client) => {
+    if (!client.isAlive) return client.terminate();
+    client.isAlive = false;
+    client.ping(null, undefined);
+  });
+}
+
+wss.on('connection', (ws) => {
   console.log('Client connected');
-
-  // 监听消息事件
-  socket.on('message', (data) => {
-    console.log(`Received message: ${data}`);
-
-    // 发送消息到客户端
-    socket.send(`You sent: ${data}`);
+  ws.isAlive = true;
+  // 处理收到的消息
+  ws.on('message', (message) => {
+    if (ws.isAuthorized) {
+        if(message == 'pang') {
+            // pang 
+        } else {
+            console.log(`Received message from authorized client: ${message}`);
+        }
+    
+    } else {
+      if (message === 'auth') {
+        ws.isAuthorized = true;
+        clients.add(ws);
+        console.log('Client authorized');
+        ws.send(`authorized`)
+      } else {
+        console.log('Client unauthorized');
+        ws.terminate();
+      }
+    }
   });
 
-  // 监听断开连接事件
-  socket.on('close', () => {
+  ws.on('error', e => {
+    console.log(e)
+    ws.send(e)
+  })
+
+  // 处理连接关闭
+  ws.on('close', () => {
     console.log('Client disconnected');
+    clients.delete(ws);
+  });
+
+  // 处理心跳检测
+  ws.on('pong', () => {
+    ws.isAlive = true;
   });
 });
 
-console.log('WebSocket server started');
+// 启动心跳检测
+setInterval(heartbeat, 30000);
